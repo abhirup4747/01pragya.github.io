f4f8d1f0c5717d75dfe7fd444d343bc926bb911c
01pragya <85448365+01pragya@users.noreply.github.com>
2021-06-07 16:58:34 +0530

    Update authenticate.js

--- resources/js/authenticate.js
+++ resources/js/authenticate.js
@@ -19,6 +19,6 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
-      window.location="../../index.html";
+      window.location="../index.html";
     });
   }
