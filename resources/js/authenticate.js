cafda15007ee52136e66fc4c317f60ef3218b48b
01pragya <85448365+01pragya@users.noreply.github.com>
2021-06-08 13:39:57 +0530

    Update authenticate.js

--- resources/js/authenticate.js
+++ resources/js/authenticate.js
@@ -14,5 +14,6 @@ function onSignIn(googleUser) {
       document.getElementById("owner").innerHTML =  profile.getGivenName() ;
       document.getElementById("disply-name").innerHTML = profile.getName();
       document.getElementById("signout").innerHTML = "<a href=\"#\" onclick=\"signOut();\">Sign out</a>";
+      document.getElementById("email").innerHTML = "Signed In as "+ profile.getEmail();
     }
 
