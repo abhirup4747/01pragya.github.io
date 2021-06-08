9ca93e94af1c0eb967d81ae312c12977c3905b28
01pragya <85448365+01pragya@users.noreply.github.com>
2021-06-08 10:19:17 +0530

    Update authenticate.js

--- resources/js/authenticate.js
+++ resources/js/authenticate.js
@@ -11,6 +11,7 @@ function onSignIn(googleUser) {
       // The ID token you need to pass to your backend:
       var id_token = googleUser.getAuthResponse().id_token;
       console.log("ID Token: " + id_token);
+      document.getElementById("intro-head").innerHTML = "Welcome to the blog of <span class='unknown'>"+ profile.getGivenName() +"</span>";
       document.getElementById("info-name").innerHTML = profile.getName();
       document.getElementById("info-detail").innerHTML = profile.getEmail() + "\n" + profile.getName();
     }
