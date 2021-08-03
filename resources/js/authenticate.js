f35e5ca4fb12821fc82fb171af159a2f3407527a
01pragya <85448365+01pragya@users.noreply.github.com>
2021-08-03 15:46:23 +0530

    Update authenticate.js

--- resources/js/authenticate.js
+++ resources/js/authenticate.js
@@ -18,14 +18,27 @@ function onSignIn(googleUser) {
     }
 
 
-/*const signInApple = async () => {      
-      try {
-           const data = await AppleID.auth.signIn();
-            console.log("success");
-      } catch ( error ) {
-           //handle error.
-            console.log("failure");
-      }
-}*/
+const signInApple = () => { 
+      alert("Hello");
+          /* window.AppleID.auth.init({
+                clientId : 'com.example.app.signin.dunkin',
+                scope : 'name email',
+                redirectURI : 'https://01pragya.github.io',
+                state : 'openssl_random_pseudo_bytes',
+               // nonce : '[NONCE]',
+                usePopup : true //or false defaults to false
+            });
+         
+         const response = async () => {      
+            try {
+                 const data = await AppleID.auth.signIn();
+                  console.log(data);
+            } catch ( error ) {
+                 //handle error.
+                  console.log("failure");
+            }
+         }
+         response();*/
+}
 
 
