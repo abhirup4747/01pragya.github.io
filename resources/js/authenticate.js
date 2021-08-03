439699a63d1254e347328f145b9d5ec307f4b7cc
01pragya <85448365+01pragya@users.noreply.github.com>
2021-08-03 17:50:43 +0530

    Update authenticate.js

--- resources/js/authenticate.js
+++ resources/js/authenticate.js
@@ -23,7 +23,7 @@ const signInApple = () => {
            window.AppleID.auth.init({
                 clientId : 'com.example.app.signin.dunkin',
                 scope : 'name email',
-                redirectURI : 'https://example-app.com/redirect',
+                redirectURI : 'https://example-app.com',
                // state : 'openssl_random_pseudo_bytes',               
                 usePopup : true //or false defaults to false
             });
