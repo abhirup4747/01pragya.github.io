694fb01dfacb4e99e03a7916dc1367eeb16e3b8f
01pragya <85448365+01pragya@users.noreply.github.com>
2021-08-04 11:55:30 +0530

    Update authenticate.js

--- resources/js/authenticate.js
+++ resources/js/authenticate.js
@@ -22,7 +22,7 @@ const signInApple = () => {
       alert("Hello");
            window.AppleID.auth.init({
                 clientId : 'com.example.app.signin.dunkin',
-                scope : 'name email',
+                scope : 'name email openid',
                 redirectURI : 'https://01pragya.github.io',
                // state : 'openssl_random_pseudo_bytes',               
                 usePopup : true //or false defaults to false
@@ -31,7 +31,8 @@ const signInApple = () => {
          const response = async () => {      
             try {
                  const data = await AppleID.auth.signIn();
-                  if(Object.keys(data).length==2){
+                  if(Object.keys(data).length>=2){
+                        console.log(data);
                         console.log(data.authorization);
                         console.log(data.user);
                   }
